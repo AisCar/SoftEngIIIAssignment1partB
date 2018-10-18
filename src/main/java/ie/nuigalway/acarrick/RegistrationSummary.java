c0654b19a3d73ce19e8069a00c60c039ac82efe6
AisCar <aislingcarrick@yahoo.ie>
2018-10-18 17:07:04 +0100

    Use proper accessor methods

--- src/main/java/ie/nuigalway/acarrick/RegistrationSummary.java
+++ src/main/java/ie/nuigalway/acarrick/RegistrationSummary.java
@@ -1,13 +1,12 @@
 
 public class RegistrationSummary {
-  //TODO use actual accessor methods - go back and write those in part A
 
   public void RegistrationSummary(){
 
   }
 
   public void printCourse(CourseProgramme course){
-    System.out.println("\nCourse: " + course.courseName);
+    System.out.println("\nCourse: " + course.getName());
     printCourseModules(course);
     //printStudents(course); //is necessary? Check requirements.
   }
@@ -15,26 +14,26 @@ public class RegistrationSummary {
 
   public void printCourseModules(CourseProgramme course){
     System.out.println("Modules:");
-    for(Module m : course.modules){
-      System.out.println("- " + m.name);
+    for(Module m : course.getModules()){
+      System.out.println("- " + m.getName());
     }
   }
 
 
   public void printStudents(CourseProgramme course){
     System.out.println("Students:");
-    for(Student s : course.enrolledStudents){
-      System.out.println("- " + s.name + " ("+ s.getUsername() + ")");
+    for(Student s : course.getStudents()){
+      System.out.println("- " + s.getName() + " ("+ s.getUsername() + ")");
     }
   }
 
 
   public void printStudent(Student student){
-    System.out.println("\nStudent: " + student.name + " (" + student.getUsername() + ")");
-    System.out.println("Course: " + student.course.courseName);
+    System.out.println("\nStudent: " + student.getName() + " (" + student.getUsername() + ")");
+    System.out.println("Course: " + student.getCourse().getName());
     String modules = "";
-    for(Module m : student.modules){
-      modules = modules + m.name +", ";
+    for(Module m : student.getModules()){
+      modules = modules + m.getName() +", ";
     }
     System.out.println("Modules: " + modules);
   }
